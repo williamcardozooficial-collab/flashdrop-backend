63bb863e79c3a1413b376928913f36c92a24c190
williamcardozooficial-collab <williamcardozooficial@gmail.com>
2026-06-21 23:49:28 -0300

    Fix syntax error in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
-const app = express();h
+const app = express();
 app.use(cors({ origin: '*', methods: ['GET','POST','PUT','DELETE','OPTIONS'], allowedHeaders: ['Content-Type','Authorization'] }));
 app.options('*', cors());
 app.use(express.json());
